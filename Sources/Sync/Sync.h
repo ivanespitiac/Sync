ab05de047b4a071f8c0cddf109c60ac3916ecc5b
Ivan Espitia <ivan.espitia@grupodigital.co>
2022-03-15 13:29:04 -0500

    ivan.espitia@uptc.edu.co
    migrate project to Swift Package Manager

---
+++ Sources/Sync/Sync.h
@@ -0,0 +1,11 @@
+@import Foundation;
+@import CoreData;
+
+FOUNDATION_EXPORT double SyncVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char SyncVersionString[];
+
+#import <Sync-iOS-Objc/PropertyMapper.h>
+#import <Sync-iOS-Objc/NSEntityDescription+PrimaryKey.h>
+#import <Sync-iOS-Objc/NSManagedObject+PropertyMapperHelpers.h>
+#import <Sync-iOS-Objc/NSPropertyDescription+Sync.h>
